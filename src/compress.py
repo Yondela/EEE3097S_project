9eb1d736a495f3016ba2464936bd59705361cd35
Yondela <yondela.matwele289@gmail.com>
2021-09-29 10:55:44 +0200

    Inital commit with read_data and filenames functions implemented.

---
+++ src/compress.py
@@ -0,0 +1,28 @@
+
+def read_data(filename):
+    """This function is used to read the contents of the file."""
+
+    with open(filename, 'r') as f:
+
+        return f.readlines()
+
+def filenames():
+
+    filenames = [ "2018-09-19-03_57_11_VN100.csv",
+        "2018-09-19-04_22_21_VN100.csv",
+        "2018-09-19-06_28_11_VN100.csv",
+        "2018-09-19-06_53_21_VN100.csv",
+        "2018-09-19-08_59_11_VN100.csv",
+        "2018-09-19-09_24_21_VN100.csv",
+        "2018-09-19-09_49_31_VN100.csv",
+        "2018-09-19-11_55_21_VN100.csv",
+        "2018-09-19-12_20_31_VN100.csv",
+    ]
+
+    return filenames
+
+
+
+if __name__ == "__main__":
+
+    pass
